d77c2f05860b2deb808749ec825e025a2fb07537
pradeep <pradeepkittu07@gmail.com>
2021-03-29 13:37:30 +0530

    commit pattern 25 27 32

---
+++ JavaWorldofEclipse/src/NumberPatterns/Pattern32.java
@@ -0,0 +1,79 @@
+package NumberPatterns;
+
+import java.util.Scanner;
+
+/**
+* total number of rows
+* work to be done in every row
+* preperation to do for the next row
+* 
+* cst==count of stars
+* nst=number of stars
+* csp=sount of spaces
+* nsp=number of spaces
+* 
+*/
+/**
+
+ans if n=5 and with 1
+
+
+ 	 	 	 	1	
+ 	 	 	1	1	1	
+ 	 	1	1	1	1	1	
+ 	1	1	1	1	1	1	1	
+1	1	1	1	1	1	1	1	1
+
+
+*/
+
+public class Pattern32 {
+
+	public static void main(String[] args) {
+		Scanner scn =new Scanner(System.in);
+		System.out.println("enter n for ur pattern");
+		int n =scn.nextInt();
+		int val=1;
+	int nst=1;
+		int nr=2*n-1;
+		int row=1;
+		for(;row<=nr;row++)
+		{
+			
+			//work
+			for(int cst=1;cst<=nst;cst++)
+			{
+				if(cst%2!=0)
+				{
+					System.out.print(val);	
+				}
+				else
+				{
+			System.out.print("*");
+				}
+			}
+		
+//		preperation for next row
+			
+			System.out.println();
+			
+			if(row<=nr/2)
+			{
+			nst=nst+2;
+			val++;
+			}
+			else
+			{
+				nst=nst-2;
+				val--;
+			}
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
